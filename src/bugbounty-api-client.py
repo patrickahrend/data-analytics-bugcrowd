066fb16d4873a71aa02057efedff8af943a2ba8e
Patrick Ahrend <patrickahrend@yahoo.com>
2022-12-13 10:27:02 +0100

    update csv and add analytics

---
+++ src/bugbounty-api-client.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    from bug_crowd.client import BugcrowdClient
+    client = BugcrowdClient('API_TOKEN')
+
+
+if __name__ == '__main__':
+    main()
--- src/main.py
+++ src/main.py
@@ -30,19 +30,6 @@ def main():
     driver = webdriver.Chrome(service=s, options=chromeOptions)
     driver.implicitly_wait(10)
 
-    # with open("programs.html", "w") as file:
-    #     driver.get("https://bugcrowd.com/programs")
-    #     driver.implicitly_wait(10)
-
-    #     # Create a BeautifulSoup object from the response
-    #     soup = BeautifulSoup(driver.page_source, "html.parser")
-    #     file.write(soup.prettify())
-    #     program_name = soup.find_all('h4', class_='bc-panel__title')
-    #     reward_range = soup.find_all('span', class_='bc-stat__title')#
-
-    #     for name, range in zip(program_name, reward_range):
-    #         print(name.get_text(), range.get_text())
-
     # Open a CSV file for writing
     with open("programs.csv", "w", newline="") as file:
         writer = csv.writer(file)
@@ -65,36 +52,47 @@ def main():
             soup = BeautifulSoup(driver.page_source, "html.parser")
 
             program_name = soup.find_all('h4', class_='bc-panel__title')
-            reward_range = soup.find_all('span', class_='bc-stat__title')
 
-            for name, range in zip(program_name, reward_range):
-                # Find the link of the program
-                program_link = name.find('a')['href']
+            # Check if there are any programs on the page
+            if len(program_name) > 0:
+                reward_range = soup.find_all('span', class_='bc-stat__title')
 
-                writer.writerow(
-                    [name.get_text(), range.get_text(), program_link])
+                for name, range in zip(program_name, reward_range):
+                    # Find the link of the program
+                    program_link = name.find('a')['href']
 
-            # Check if there is a "Next" button on the page
-            # next_button = soup.find_all(
-            #     "button", {"class": "bc-pagination__link", "rel": "next"})
-            # print(next_button)
+                    writer.writerow(
+                        [name.get_text(), range.get_text(), program_link])
 
-            next_button = driver.find_element(
-                By.CLASS_NAME, "bc-pagination__link")
+                # Find the "Next" button
+                next_button = driver.find_element(
+                    By.CLASS_NAME, "bc-pagination__link")
 
-            page_element = driver.find_element(
-                By.CLASS_NAME, "bc-pagination__total")
+                print("Check this out", next_button.text)
+                # Get the pagination links at the bottom of the page
+                pagination_links = driver.find_elements(
+                    By.CLASS_NAME, "bc-pagination__number")
 
-            page_count = page_element.get_attribute("value")
+                # If there are no pagination links, set pagination to False
+                if len(pagination_links) == 0:
+                    pagination = False
 
-            # If there is no "Next" button, set pagination to False
-            if int(page_count) == page:
-                pagination = False
+                # If there are pagination links, get the page count from the last link
+                else:
+                    page_count = int(pagination_links[-1].text)
+
+                    # If the current page is the last page, set pagination to False
+                    if page == page_count:
+                        pagination = False
+                    else:
+                        # If there is a "Next" button, increment the page number and click it
+                        page += 1
+                        print("Currently crawling Page:  "+str(page))
+                        next_button.click()
+
+            # If there are no programs on the page, set pagination to False
             else:
-                # If there is a "Next" button, increment the page number
-                next_button.click()
-                page += 1
-                print("Currently crawling Page:  "+str(page))
+                pagination = False
 
 
 if __name__ == "__main__":
