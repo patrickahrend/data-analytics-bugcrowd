87236f27ddce5f335d137769e51450e6248a4260
Patrick Ahrend <patrickahrend@yahoo.com>
2023-01-10 16:56:20 +0100

    remove unnecessary methods

--- src/ols.py
+++ src/ols.py
@@ -8,98 +8,6 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
-def cleansing_Number_People(df: pd.DataFrame):
-
-    # Read in the data
-    df = pd.read_excel(
-        '/Users/patrickahrend/Developer/data-analytics-bugcrowd/data-octa/Bugcrowd_industry-asc_24.12.xlsx')
-
-    df = df.dropna(subset=['Number_people'])
-
-    df["Number_people"] = df["Number_people"].str.replace('total', '')
-    # Print the first few rows
-    df['Number_people'] = df['Number_people'].astype(int)
-
-    return df
-
-
-def clean_Hall_of_Fame(df: pd.DataFrame):
-    df = df.dropna(subset=['Hall_of_famers'])
-
-    df["Hall_of_famers"] = df["Hall_of_famers"].str.replace(
-        'View the hall', '')
-    df["Hall_of_famers"] = df["Hall_of_famers"].str.replace('View all ', '')
-    df["Hall_of_famers"] = df["Hall_of_famers"].str.replace('', '0')
-    df["Hall_of_famers"] = df["Hall_of_famers"].astype(int)
-
-    return df
-
-# TODO: fix this methode to clean the data and use in OLS
-
-
-def cleasne_vulnearbilities_rewarded(df: pd.DataFrame):
-    df['Vulnearbilities_rewarded'] = df['Vulnearbilities_rewarded'].astype(int)
-    return df
-
-
-def cleasne_validation_within(df: pd.DataFrame):
-
-    df["Validation_within"] = df["Validation_within"].str.replace(
-        '\n', '').str.replace('days', '')
-
-    for ele in df["Validation_within"]:
-        if "$" in ele:
-            ele = ele.replace(ele, "0")
-            ele = ele.strip()
-        if "days" in ele:
-            ele = ele.replace("days", "")
-            ele = ele.strip()
-            ele = int(ele)
-            ele = ele * 24
-            ele = str(ele) + " hours"
-        if "day" in ele:
-            ele = ele.replace("day", "")
-            ele = ele.strip()
-            ele = int(ele)
-            ele = ele * 24
-            ele = str(ele) + " hours"
-        if "about" in ele:
-            ele = ele.replace("about", "")
-            ele = ele.strip()
-        if "month" in ele:
-            ele = ele.replace("month", "")
-            ele = ele.strip()
-            ele = int(ele)
-            ele = ele * 24 * 30
-            ele = str(ele) + " hours"
-        if "minutes" in ele:
-            ele = ele.replace("minutes", "")
-            ele = ele.strip()
-            ele = int(ele)
-            ele = ele * 60
-            ele = str(ele) + " hours"
-        if "hours" in ele:
-            ele = ele.replace("hours", "")
-            ele = ele.strip()
-        if "hour" in ele:
-            ele = ele.replace("hour", "")
-            ele = ele.strip()
-
-        df_final = df_final.append(
-            {'Validation_within': ele}, ignore_index=True)
-    return df
-
-
-def clean_average_payout(df: pd.DataFrame):
-
-    df = df.dropna(subset=['Average_payout'])
-    df["Average_payout"] = df["Average_payout"].str.replace(
-        '$', '').str.replace(',', '').str.replace(' ', '')
-    df["Average_payout"] = df["Average_payout"].str.replace("", "0")
-    df["Average_payout"] = df["Average_payout"].astype(float)
-    return df
-
-
 def ols(df: pd.DataFrame):
     # Create a linear regression object
     model = LinearRegression()
@@ -169,6 +77,9 @@ def main():
     df["Average Payout"] = df["Average Payout"].replace(0, np.nan)
     df["Number People"] = df["Number People"].replace(0, np.nan)
     df["Hall of Famers"] = df["Hall of Famers"].replace(0, np.nan)
+
+    # looking at the smaller programs
+    df = df.where(df['Vulnearbilities Rewarded'] <= 500)
 
     df = df.dropna(subset=['Hall of Famers', 'Number People',
                            'Validation Within', 'Average Payout', 'Vulnearbilities Rewarded'])
